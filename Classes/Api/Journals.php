ab1a61d6b48503bf5da450b8cc69c75ca329b46d
Lucas Thurston <lucas@castironcoding.com>
2022-01-13 08:47:46 -0800

    [C] Add more robust routing

--- Classes/Api/Journals.php
+++ Classes/Api/Journals.php
@@ -6,8 +6,18 @@ use CdlExportPlugin\Utility\Traits\DAOCache;
 class Journals {
     use DAOCache;
 
-    public function execute()
+    public function execute($args)
     {
+        return @$args['journal'] ? $this->getJournal($args['journal']) : $this->getJournals();
+    }
+
+    private function getJournal($id) {
+        $journal = $this->getDAO('journal')->getJournal($id);
+        if(is_null($journal)) throw new \Exception("Journal $id not found");
+        return DataObjectUtility::dataObjectToArray($journal);
+    }
+
+    private function getJournals() {
         $journalsResultSet = $this->getDAO('journal')->getJournals();
         $data = [];
         foreach ($journalsResultSet->toArray() as $journal) {
--- Classes/Command/Api.php
+++ Classes/Command/Api.php
@@ -8,29 +8,77 @@ class Api {
     private $args = [];
 
     private $routes = [
-        // Don't use a ~ character in these regexes, unless you escape them, kew? We're using them
-        // as the wrapping token.
-        '^/journals$' => \CdlExportPlugin\Api\Journals::class,
-        '^/journals/(\d+)$' => \CdlExportPlugin\Api\Journals::class,
-        '^/journals/(\d+)/sections$' => \CdlExportPlugin\Api\Journals\Sections::class,
+        // Don't use a ~ character in these route regexes, unless you escape them, kew? We're using them
+        // as the delimiter. Note we're using named parameters too.
+        '^/journals(/(?P<journal>\d+))?$' => \CdlExportPlugin\Api\Journals::class,
+        '^/journals/(?P<journal>\d+)/sections(/(?P<section>\d+))?$' => \CdlExportPlugin\Api\Journals\Sections::class,
         '^/journals/(\d+)/issues$' => \CdlExportPlugin\Api\Journals\Issues::class,
         '^/journals/(\d+)/articles$' => \CdlExportPlugin\Api\Journals\Articles::class,
     ];
 
+    /**
+     * Api constructor.
+     * @param $args
+     */
     public function __construct($args) {
         $this->args = $args;
     }
 
-    function execute() {
+    /**
+     *
+     */
+    public function execute() {
+        $out = ['Exception' => ["Route '".$this->args[0]."' did not match defined routes"]]; // Fallback response
         foreach($this->routes as $route => $class) {
             $matches = [];
             if(preg_match('~'.$route.'~', $this->args[0], $matches)) {
-                $apiClassInstance = new $class($matches);
-                echo json_encode($apiClassInstance->execute());
-                die();
+                $out = $this->callApiMethod($route, $class, $matches);
+                break;
             }
         }
+        echo json_encode($out).PHP_EOL;
+    }
 
-        echo json_encode(['404' => ['A 404 on the command line?!']]);
+    /**
+     * Call API method, catch exceptions
+     * @param $route
+     * @param $class
+     * @param $matches
+     * @return array|mixed
+     */
+    private function callApiMethod($route, $class, $matches) {
+        $args = $this->zipArgs($matches, $this->getRegexNamedMatches($route));
+
+        try {
+            $out = (new $class($matches))->execute($args);
+        } catch(\Exception $e) {
+            // TODO: this could be make more robust exception handling
+            $out = ['Exception' => $e->getMessage()];
+        }
+        return $out;
+    }
+
+    /**
+     * Extract the named matches from a regex, with a regex!
+     * @param $route
+     * @return array|mixed
+     */
+    private function getRegexNamedMatches($route) {
+        return preg_match_all('/\?P<([a-zA-Z0-9_]+)>/', $route, $matches) ? $matches[1] : [];
+    }
+
+    /**
+     * Given an array of matches and an array of allowed params, return just the params from the matches that are
+     * allowed
+     * @param $rawArgs
+     * @param $allowedParams
+     * @return array
+     */
+    private function zipArgs($rawArgs, $allowedParams) {
+        $out = [];
+        foreach($allowedParams as $allowedParam) {
+            $out[$allowedParam] = @$rawArgs[$allowedParam];
+        }
+        return $out;
     }
 }
