9478857cd4cfa2c9941aded66677c93faeef6734
Lucas Thurston <lucas@castironcoding.com>
2022-04-06 13:18:44 -0700

    [C] Cast dates to UTC

--- Classes/Utility/DateUtility.php
+++ Classes/Utility/DateUtility.php
@@ -8,6 +8,7 @@ class DateUtility {
      */
     public static function formatDateString($dateString) {
         return (new \DateTime($dateString, new \DateTimeZone('America/Los_Angeles')))
+            ->setTimezone(new \DateTimeZone('UTC'))
             ->format('c');
     }
 }
