75d490788a4b3c10506b4c94c3fb668eba97b6ef
Lucas Thurston <lucas@castironcoding.com>
2022-09-21 16:44:40 -0700

    [B] Adjust plugin routing to handle OJS running in dir

--- Classes/JournalTransporterAPIHandler.php
+++ Classes/JournalTransporterAPIHandler.php
@@ -14,7 +14,8 @@ class JournalTransporterAPIHandler extends Handler {
      * @param $class
      */
     protected function handleRequest($class) {
-        $requestUri = '/' . implode('/', array_slice(explode('/', $_SERVER['REQUEST_URI']), 5));
+        $requestUri = preg_replace('/^.*\/index\.php\/pages\/jt\/api(.*)$/', '$1', $_SERVER['REQUEST_URI']);
+
         $api = new $class([$requestUri]);
         $response = $api->execute();
 
